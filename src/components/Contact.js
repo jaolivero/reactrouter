bfd18fa480adc44a1e40220ad79b9319716e946a
Joel Olivero <joelolivero.student@careerdevs.com>
2020-04-13 14:28:20 -0400

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>contact page for website</p>
+            </div>
+    )
+}
+export default Contact
