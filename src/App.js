569e6ec30bd75541d2066775ece185b013c5667a
joel olivero <joelolivero@outlook.com>
2020-04-13 15:47:47 -0400

    New CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-  
-import React, { Component } from 'react';
-import Navbar from './components/Navbar'
-import { Route, BrowserRouter } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Post from './components/Post'
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Post from "./components/Post";
 
 class App extends Component {
   render() {
@@ -13,10 +12,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path="/:post_id" component={Post} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
